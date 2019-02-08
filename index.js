2776edd46a8f6286ed442ad59c1b703d4b332cac
Hidenori Fujimura <hfu@users.noreply.github.com>
2019-02-07 21:33:59 -0500

    update

---
+++ index.js
@@ -0,0 +1,41 @@
+const config = require('config')
+const { spawnSync } = require('child_process')
+const tilebelt = require('@mapbox/tilebelt')
+const fs = require('fs')
+
+// configuration constants
+const z = config.get('z')
+const minx = config.get('minx')
+const miny = config.get('miny')
+const maxx = config.get('maxx')
+const maxy = config.get('maxy')
+const configPath = config.get('configPath')
+const planetPath = config.get('planetPath')
+
+let configuration = {
+  extracts: [],
+  directory: 'pbf'
+}
+
+for (let x = minx; x <= maxx; x++) {
+  for (let y = miny; y <= maxy; y++) {
+    configuration.extracts.push({
+      output: `${z}-${x}-${y}.pbf`,
+      output_format: 'pbf',
+      bbox: tilebelt.tileToBBOX([x, y, z])
+    })
+  }
+}
+
+fs.writeFileSync(
+  configPath,
+  JSON.stringify(configuration, null, 2)
+)
+
+spawnSync('osmium', [
+  'extract', '--config', configPath, 
+  '--strategy=smart', 
+  '--verbose', '--overwrite', '--progress', 
+  planetPath],
+  { stdio: 'inherit' }
+)
